99042adf96461970ddec42ab34fad9f9a44a7776
Sachin Somia <sachinsomia@gmail.com>
2022-05-20 00:28:11 +0100

    moves to vitest

--- tests/lib/notification.test.ts
+++ tests/lib/notification.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest'
 import { Low } from 'lowdb/lib';
 import { Data } from '../../src/lib/db';
 import { makeMessageArray} from '../../src/lib/notification'
