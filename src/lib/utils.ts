15b0dff09d482e6ea2a791814baab9237970c943
Sachin Somia <sachinsomia@gmail.com>
2022-05-23 20:22:55 +0100

    Refactor: adds validation for scrapeutils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-
+import { z } from 'zod'
 import { Low } from "lowdb/lib";
-import { Data } from "./db";
+import { Data, RecordedPrice } from "./db";
 
 
 export async function getHTML(url:string) {
@@ -10,26 +10,59 @@ export async function getHTML(url:string) {
   return data
 }
 
-export async function getTescoPrice(url:string): Promise<Number> {
-  const data = await getHTML(url)
-  const $ = cheerio.load(data)
-  const price = $(".price-per-sellable-unit .value")
-  return parseFloat(price.text())
+interface ScrapedPrice {
+  location: "tesco" | "dunnes" | "supervalu",
+  price: number
 }
 
-export async function getDunnesPrice(url:string): Promise<Number> {
-  const data = await getHTML(url)
-  const $ = cheerio.load(data)
-  const price = $("[itemprop=price]")[0].attribs.content
-  return parseFloat(price.slice(1))
+function validatePrice(p:unknown): number {
+  const validatedPrice = z.number().safeParse(p)
+  return validatedPrice.success ? validatedPrice.data : -1
 }
 
-export async function getSuperValuPrice(url: string | undefined): Promise<Number | undefined> {
-  if (!url) return
-  const data = await getHTML(url)
-  const $ = cheerio.load(data)
-  const price = $(".product-details-price-item")
-  return parseFloat(price.text().trim().slice(1))
+export async function getTescoPrice(url:string): Promise<ScrapedPrice> {
+  try {
+    const data = await getHTML(url)
+    const $ = cheerio.load(data)
+    const priceElement = $(".price-per-sellable-unit .value")
+    const p = parseFloat(priceElement.text())
+    const price = validatePrice(p)
+    return { location : "tesco", price }
+  } catch {
+    return  { location : "tesco", price: -1 }
+  }
+}
+
+export async function getDunnesPrice(url:string): Promise<ScrapedPrice> {
+  try {
+    const data = await getHTML(url)
+    const $ = cheerio.load(data)
+    const priceElement = $("[itemprop=price]")[0].attribs.content
+    const p = parseFloat(priceElement.slice(1))
+    const price = validatePrice(p)
+    return { location : "dunnes", price }
+  } catch {
+    return { location : "dunnes", price: -1 }
+  }
+}
+
+export async function getSuperValuPrice(url: string): Promise<ScrapedPrice> {
+  try {
+    const data = await getHTML(url)
+    const $ = cheerio.load(data)
+    const priceElement = $(".product-details-price-item")
+    const p = parseFloat(priceElement.text().trim().slice(1))
+    const price = validatePrice (p)
+    return {location: "supervalu", price}
+  } catch {
+    return { location: "supervalu", price: -1 }
+  }
+}
+
+const scrapeFunctions = {
+  tesco: getTescoPrice,
+  dunnes: getDunnesPrice,
+  supervalu: getSuperValuPrice
 }
 
 export async function scrapePrices(db: Low<Data>) {
@@ -52,16 +85,19 @@ export async function scrapePrices(db: Low<Data>) {
           break
       }
     }
-    const [tPrice, dPrice, sPrice] = await Promise.all([tPromise, dPromise, sPromise])
-    const priceObj = {
+    const scrapedPricePromises = await Promise.allSettled([tPromise, dPromise, sPromise])
+    const priceObj: RecordedPrice = {
       date: Date.now(),
-      prices: {
-        tesco: tPrice,
-        dunnes: dPrice,
-        supervalu: sPrice
-      }
+      prices: {}
     }
-    //@ts-ignore // Lowdb uses JSON.stringify which discard undefined values
+
+    for (let scrapedPricePromise of scrapedPricePromises) {
+      if (scrapedPricePromise.status === 'rejected') continue
+      if (!scrapedPricePromise.value) continue
+      if (scrapedPricePromise.value.price < 0) continue
+      priceObj.prices[scrapedPricePromise.value.location] = scrapedPricePromise.value.price
+    }
+    
     item.recordedPrices.push(priceObj)
     db.write()
   }
