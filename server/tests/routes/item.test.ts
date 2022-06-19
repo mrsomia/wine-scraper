be45061ca7df276cf7b10f5dced928637d2bec8a
Sachin Somia <46264992+mrsomia@users.noreply.github.com>
2022-06-19 20:38:59 +0100

    Feat: Get all prices records for an item (#17)
    Implements server route to get all prices records for an item

--- server/tests/routes/item.test.ts
+++ server/tests/routes/item.test.ts
@@ -1,10 +1,44 @@
+import { after } from "cheerio/lib/api/manipulation";
 import { describe, expect, test, vi, afterEach } from "vitest";
 import { fastify } from "../../src/index";
-import { createItem } from "../../src/lib/dao";
+import { createItem, getAllItemsPrices } from "../../src/lib/dao";
+
+const expectedFromGetAllItems = {
+    "id": 1,
+    "name": "19 Crimes Dark Red Wine",
+    "urlsId": 1,
+    "prices": [
+      {
+        "id": 1,
+        "dateTime": "2022-04-10T15:57:26.476Z",
+        "tesco": 15,
+        "dunnes": 15,
+        "supervalu": null,
+        "itemId": 1
+      },
+      {
+        "id": 2,
+        "dateTime": "2022-04-10T23:44:01.029Z",
+        "tesco": 10,
+        "dunnes": 9,
+        "supervalu": null,
+        "itemId": 1
+      },
+      {
+        "id": 5,
+        "dateTime": "2022-06-06T00:22:01.222Z",
+        "tesco": 15,
+        "dunnes": 9.6,
+        "supervalu": null,
+        "itemId": 1
+      }
+    ]
+  }
 
 vi.mock("../../src/lib/dao", () => {
   return {
     createItem: vi.fn(),
+    getAllItemsPrices: vi.fn().mockImplementation((id) => id === 1 ? expectedFromGetAllItems : undefined)
   };
 });
 
@@ -60,3 +94,40 @@ describe("Tests add new item route", () => {
     expect(response.statusCode).toBe(400)
   })
 });
+
+describe("tests get all price records of an item route", ()=> {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  test("should get all item price records", async ()=> {
+    const payload = {
+      id: 1
+    }
+
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/item/get-all-prices",
+      payload
+    })
+
+    expect(getAllItemsPrices).toBeCalled()
+    expect(JSON.parse(response.body)).toEqual({message: "Success", item: {...expectedFromGetAllItems}})
+  })
+
+  test("should return error with incorrect input", async () => {
+    const payload = {
+      id: '1'
+    }
+
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/item/get-all-prices",
+      payload
+    })
+
+    expect(getAllItemsPrices).not.toBeCalled();
+    expect(response.json()).toMatchObject({ message: 'Error'})
+    expect(response.statusCode).toBe(400)
+  })
+})
