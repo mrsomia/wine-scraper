602c1dcf60dbcda9c2f9700be821148ccaa217e4
Sachin Somia <sachinsomia@gmail.com>
2022-06-21 21:14:36 +0100

    moves to a reducer for state

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,16 +1,50 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import styles from  "../styles/UserHome.module.css"
 import { ItemProps, Item } from '../components/Item'
+// import { initialState, stateReducer } from '../utils/context'
+
+interface InitialState {
+  user: {
+    name: string,
+    id: number,
+    // favourites: ItemProps[],
+  },
+  items: null | ItemProps[],
+  activeItem: null | ItemProps
+}
+
+export const initialState: InitialState = {
+  user: {
+    name: 'Testy McTest face',
+    id: 1,
+    // favourites: []
+  },
+  items: null,
+  activeItem: null
+}
+
+
+export function stateReducer(state: typeof initialState, action: ACTIONTYPE) {
+  switch(action.type) {
+    case 'UPDATE-ITEMS':
+      return { ...state, items: action.payload }
+    default:
+      return state
+      throw new Error("bad action type")
+  }
+}
+
+type ACTIONTYPE = | { type: 'UPDATE-ITEMS', payload: ItemProps[] }
 
 function UserHome(): JSX.Element {
-  const [items, setItems] = useState<null | ItemProps[]>(null)
+  const [state, dispatch] = useReducer(stateReducer, initialState)
 
   const getItems = useEffect(() => {
     async function getData() {
       try {
         const res = await fetch('http://localhost:8080/item-prices')
         const data = await res.json()
-        setItems(data)
+        dispatch( {type: 'UPDATE-ITEMS', payload: data })
       } catch (e) {
         console.log(e)
       }
@@ -18,12 +52,12 @@ function UserHome(): JSX.Element {
     getData()
   }, [])
 
-  console.log(items)
+  console.log(state)
 
   return (<div className={styles['user-favourites']}>
     <main>
       <h1>Items</h1>
-      { items ? items.map(item => (
+      { state.items ? state.items.map(item => (
         <Item   {...item} key={item.id} />
       ))
        :
