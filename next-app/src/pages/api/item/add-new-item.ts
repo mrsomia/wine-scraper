e2a12a3a2b22fda324da3687b2b763840c527d6d
Sachin Somia <sachinsomia@gmail.com>
2022-08-22 20:18:51 +0100

    Refactor: adds add-new-item route to next

--- next-app/src/pages/api/item/add-new-item.ts
+++ next-app/src/pages/api/item/add-new-item.ts
@@ -1,13 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
 
+import { createItem } from '../../../lib/db'
 // type Data = {
 //   name: string
 // }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json(req.body)
+  const { body: item } = req
+  let Item = z.object({
+    name: z.string(),
+    urls: z.object({
+      tesco: z.string(),
+      dunnes: z.string(),
+      supervalu: z.string()
+    }).partial()
+  })
+
+  const validated = Item.safeParse(item)
+  if (validated.success) {
+    const item  = await createItem(validated.data.name, validated.data.urls)
+    // TODO: check if item exists already
+
+    res.status(201).json({
+      item,
+      message: 'Success',
+    })
+  } else {
+    const { error } = validated
+    res.status(400)
+      .send({
+      message: 'Error',
+      error: error.issues
+    })
+  }
 }
