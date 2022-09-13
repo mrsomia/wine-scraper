419296292bca6fd0ef090557f30d1d80acc822cb
Sachin Somia <sachinsomia@gmail.com>
2022-09-13 18:54:35 +0100

    Feat: Adds custom login page

--- next-app/src/pages/login.tsx
+++ next-app/src/pages/login.tsx
@@ -1,6 +1,13 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { getProviders, useSession, signIn, signOut, type ClientSafeProvider } from "next-auth/react"
+import Navbar from "../components/navbar"
 
-export default function Component() {
+interface LoginProps {
+  providers: {
+    [s: string]: ClientSafeProvider
+  }
+}
+
+export default function Login({ providers }: LoginProps) {
   const { data: session } = useSession()
   if (session) {
     return (
@@ -12,8 +19,26 @@ export default function Component() {
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <Navbar />
+      <main className="flex justify-center content-center min-h-screen">
+        <div className="flex flex-col content-center justify-center gap-5">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button onClick={() => signIn(provider.id)}>
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   )
+}
+
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: { providers },
+  }
 }
