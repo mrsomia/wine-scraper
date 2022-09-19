0ab4ecbd72bb167f5cbc4181664b7042de5f5944
Sachin Somia <sachinsomia@gmail.com>
2022-09-19 14:08:28 +0100

    Feat: Adds scraper but WIP error handling

---
+++ next-app/src/pages/api/scrape-new-prices.ts
@@ -0,0 +1,155 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+import * as cheerio from "cheerio";
+import axios from "axios";
+import { type PriceRecord, type Urls, type Item } from "@prisma/client";
+
+import { getAllItemsAndUrls, addNewPrice } from "../../lib/db";
+
+// type Data = {
+//   name: string
+// }
+
+type location = "tesco" | "dunnes" | "supervalu";
+
+interface ScrapedPrice {
+  location: location;
+  price: number;
+}
+
+async function getHTML(url: string) {
+  const { data } = await axios.get(url);
+  return data;
+}
+
+function validatePrice(p: unknown): number {
+  return z.number().parse(p);
+}
+
+async function getTescoPrice(url: string): Promise<ScrapedPrice> {
+  try {
+    const data = await getHTML(url);
+    const $ = cheerio.load(data);
+    const priceElement = $(".price-per-sellable-unit .value");
+    const p = parseFloat(priceElement.text());
+    const price = validatePrice(p);
+    return { location: "tesco", price };
+  } catch (e) {
+    console.error(e);
+    return { location: "tesco", price: -1 };
+  }
+}
+
+async function getDunnesPrice(url: string): Promise<ScrapedPrice> {
+  try {
+    const data = await getHTML(url);
+    const $ = cheerio.load(data);
+    const priceElement = $("[itemprop=price]")[0].attribs.content;
+    const p = parseFloat(priceElement.slice(1));
+    const price = validatePrice(p);
+    return { location: "dunnes", price };
+  } catch (e) {
+    console.error(e);
+    return { location: "dunnes", price: -1 };
+  }
+}
+
+async function getSuperValuPrice(url: string): Promise<ScrapedPrice> {
+  try {
+    const data = await getHTML(url);
+    const $ = cheerio.load(data);
+    const priceElement = $(".product-details-price-item");
+    const p = parseFloat(priceElement.text().trim().slice(1));
+    const price = validatePrice(p);
+    return { location: "supervalu", price };
+  } catch (e) {
+    console.error(e);
+    return { location: "supervalu", price: -1 };
+  }
+}
+
+const scrapeFunctions = {
+  tesco: getTescoPrice,
+  dunnes: getDunnesPrice,
+  supervalu: getSuperValuPrice,
+};
+
+async function createArrayOfScrapePromises(
+  item: Item & { urls: Urls }
+): Promise<PromiseSettledResult<ScrapedPrice>[]> {
+  let scrapePromises: Promise<ScrapedPrice>[] = [];
+
+  for (let itemurl of Object.entries(item.urls)) {
+    let [prop, url] = itemurl;
+    if (prop === "tesco" || prop === "dunnes" || prop === "supervalu") {
+      let func = scrapeFunctions[prop];
+      if (typeof url === "string") scrapePromises.push(func(url));
+    }
+  }
+  const scrapedPricePromises = await Promise.allSettled(scrapePromises);
+  return scrapedPricePromises;
+}
+
+function createPriceObj(
+  scrapedPricePromises: PromiseSettledResult<ScrapedPrice>[]
+) {
+  const priceObj: Partial<PriceRecord> = {
+    dateTime: new Date(),
+  };
+
+  for (let scrapedPricePromise of scrapedPricePromises) {
+    if (scrapedPricePromise.status === "rejected") continue;
+    if (!scrapedPricePromise.value) continue;
+    if (scrapedPricePromise.value.price < 0)
+      priceObj[scrapedPricePromise.value.location] = null;
+    priceObj[scrapedPricePromise.value.location] =
+      scrapedPricePromise.value.price;
+  }
+  return priceObj;
+}
+
+function isNewPriceRecord(priceObj: any): priceObj is Omit<PriceRecord, "id"> {
+  // Does not check type correctly, just that the one of the locations is
+  // present and not all Prisma type has all location with optionals as null
+  // from the DB)
+  return (
+    "dateTime" in priceObj &&
+    ("supervalu" in priceObj || "tesco" in priceObj || "dunnes" in priceObj) &&
+    "itemId" in priceObj
+  );
+}
+
+export async function scrapePricesAndAddToDB() {
+  const items = await getAllItemsAndUrls();
+  const priceObjs = await Promise.all(
+    items.map(async (item) => {
+      const scrapedPricePromises = await createArrayOfScrapePromises(item);
+      const priceObj = createPriceObj(scrapedPricePromises);
+      priceObj.itemId = item.id;
+      if (isNewPriceRecord(priceObj)) {
+        return priceObj;
+        // TODO: remove above line and comment out below
+        // addNewPrice(priceObj);
+      }
+    })
+  );
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Handle differnt methods
+  // TODO: Add validation this comes from a verified source
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).json({
+      message: "Error",
+      error: `HTTP method ${req.method} is not permitted`,
+    });
+  }
+
+  await scrapePricesAndAddToDB();
+  res.status(200).json({ message: "Success" });
+}
